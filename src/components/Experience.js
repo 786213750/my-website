43fa4111f4005f7bb356243e17a0a5f2a20803a8
786213750a <thomas@eonmedia.ai>
2023-03-15 16:29:04 -0700

    added slick and fix single scroll

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,49 +1,39 @@
 import React from "react";
-import { useTrail, animated } from 'react-spring';
 import { useNav } from '../customHooks/useNav';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './Page.css';
 
-const config = { mass: 5, tension: 2000, friction: 500 };
 
 const Experience = ({ experiences }) => {
-  const trail = useTrail(experiences.length, {
-    config,
-    opacity: 1,
-    x: 0,
-    height: 80,
-    from: { opacity: 0, x: 20, height: 0 },
-  });
+
   const expRef = useNav('Experience');
   return (
     <section ref={expRef} id='expContainer'>
       <h1>Experience</h1>
       <div className="experience-container">
-        {trail.map(({ x, height, ...rest }, index) => (
-          <animated.div
-            key={experiences[index].company}
-            className="experience-item"
-            style={{
-              ...rest,
-              transform: x.to((x) => `translate3d(0,${x}px,0)`),
-            }}
-          >
-            <a href={experiences[index].website} target="_blank" rel="noopener noreferrer">
-              <h3>{experiences[index].position}</h3>
-              <p>
-                {experiences[index].company} - {experiences[index].location}
-              </p>
-            </a>
-            <ul>
-              {experiences[index].bullets.map((responsibility, idx) => (
-                <li key={idx}>{responsibility}</li>
-              ))}
-            </ul>
-          </animated.div>
-        ))}
+        <Slider dots={true} infinite={true} speed={500} slidesToShow={1} slidesToScroll={1}>
+          {experiences.map((experience, index) => (
+            <div className='experience-content' key={index}>
+              <a href={experiences[index].website} target="_blank" rel="noopener noreferrer">
+                <h3>{experiences[index].position}</h3>
+                <p>
+                  {experiences[index].company} - {experiences[index].location}
+                </p>
+              </a>
+              <ul>
+                {experiences[index].bullets.map((responsibility, idx) => (
+                  <li key={idx}>{responsibility}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </Slider>
       </div>
     </section>
   );
-};
+}
 
 
 export default Experience;
